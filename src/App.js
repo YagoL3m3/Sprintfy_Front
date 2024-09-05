96dc986ea33546f0c9a16bcf25162c42b22ea576
Bruno Bomfim Lima <81599298+Bruno23111@users.noreply.github.com>
2024-09-04 21:20:25 -0300

    criacaoProjeto
    Criacao do projeto em React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Cabecalho from './Componentes/Cabecalho';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Cabecalho/>
+    </div>
+  );
+}
+
+export default App;
